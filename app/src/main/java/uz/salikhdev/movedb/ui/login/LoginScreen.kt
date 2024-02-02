f51b4e279c4412ee7a0696f3d69cafb85d266f0c
salikhdev <salikhdev@gmail.com>
2024-02-02 15:17:33 +0500

    :art: Commit

--- app/src/main/java/uz/salikhdev/movedb/ui/login/LoginScreen.kt
+++ app/src/main/java/uz/salikhdev/movedb/ui/login/LoginScreen.kt
@@ -1,6 +1,7 @@
 package uz.salikhdev.movedb.ui.login
 
 import android.view.View
+import android.widget.Toast
 import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.salikhdev.movedb.R
 import uz.salikhdev.movedb.core.common.BaseFragment
@@ -12,6 +13,12 @@ class LoginScreen : BaseFragment(R.layout.screen_login) {
     private val binding by viewBinding(ScreenLoginBinding::bind)
 
     override fun onViewCreated(view: View) {
+
+
+        binding.root.setOnClickListener {
+            Toast.makeText(context, "aa", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 
